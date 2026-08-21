d67176fe4f5ed0f2f780f4d654460883c4322e19
TheRealMr404 <295184324+TheRealMr404@users.noreply.github.com>
2026-08-21 22:25:08 +0330

    Add panel folder

--- index.php
+++ index.php
@@ -5088,7 +5088,6 @@ elseif (strpos($datain, "user_select_crypto_") === 0) {
     deletemessage($from_id, $message_id);
     sendmessage($from_id, $textbotlang['users']['Balance']['linkpayments'], $keyboard, 'HTML');
 
-    // ثبت تراکنش در دیتابیس Payment_report
     $dateacc = date('Y/m/d H:i:s');
     $randomString = bin2hex(random_bytes(5));
     $invoice = "{$user['Processing_value_tow']}|{$user['Processing_value_one']}";
@@ -5099,24 +5098,17 @@ elseif (strpos($datain, "user_select_crypto_") === 0) {
     $stmt->bind_param("sssssss", $from_id, $randomString, $dateacc, $user['Processing_value'], $payment_Status, $Payment_Method, $invoice);
     $stmt->execute();
 
-// ۱. دریافت نرخ واحد ارز به تومان و محاسبه مقدار نهایی
     $sym_upper = strtoupper($sym);
     $unit_rate = $rates[$sym_upper] ?? ($rates['USD'] ?? 1);
     $crypto_calc_amount = round($user['Processing_value'] / $unit_rate, ($sym_upper == 'USDT' ? 2 : 4));
 
-    // ۲. دریافت متن جزئیات ارز و ولت
     $rendered_crypto_msg = render_crypto_message($info, $user['Processing_value'], $crypto_calc_amount, $unit_rate);
 
-    // ۳. چینش ساختار نهایی متن پیام
     $textnowpayments = "<tg-emoji emoji-id=\"5350572310627632617\">✅</tg-emoji> <b>تراکنش شما ایجاد شد</b>\n\n" .
         "<tg-emoji emoji-id=\"5348498060466996739\">🛒</tg-emoji> کد پیگیری: <code>$randomString</code>\n\n" .
         $rendered_crypto_msg . "\n\n" .
         "<tg-emoji emoji-id=\"5348418461838098123\">💲</tg-emoji> مبلغ معادل به دلار: <b>$usdprice USD</b>";
 
-    // در صورتی که ارز انتخابی غیر از ترون باشد، نمایش معادل ترون نیز در انتها اضافه شود
-    if ($sym_upper != 'TRX') {
-        $textnowpayments .= "\n<tg-emoji emoji-id=\"5348418461838098123\">💲</tg-emoji> مبلغ معادل به ترون: <b>$trxprice TRX</b>";
-    }
 
     $paymentkeyboard = json_encode([
         'inline_keyboard' => [
@@ -5128,21 +5120,7 @@ elseif (strpos($datain, "user_select_crypto_") === 0) {
 
     $sent_msg = sendmessage($from_id, $textnowpayments, $paymentkeyboard, 'HTML');
     updatePaymentMessageId($sent_msg, $randomString);
-    // ارسال راهنمای پرداخت در صورت وجود
-    $gethelp = getPaySettingValue('helpofflinearze');
-    if ($gethelp !== null && $gethelp != 2) {
-        $data_help = json_decode($gethelp, true);
-        if ($data_help['type'] == "text") {
-            sendmessage($from_id, $data_help['text'], null, 'HTML');
-        } elseif ($data_help['type'] == "photo") {
-            sendphoto($from_id, $data_help['photoid'], null);
-        } elseif ($data_help['type'] == "video") {
-            sendvideo($from_id, $data_help['videoid'], null);
-        }
-    }
-
-    $sent_msg = sendmessage($from_id, $textnowpayments, $paymentkeyboard, 'HTML');
-    updatePaymentMessageId($sent_msg, $randomString);
+ 
 
 } elseif ($user['step'] == "tunnel_step_ip") {
     $ip = trim($text);
