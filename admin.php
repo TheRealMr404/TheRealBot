dcd824fef2ad21d1792006db15ab0081e80b52ce
TheRealMr404 <295184324+TheRealMr404@users.noreply.github.com>
2026-09-17 20:45:29 +0330

    Add panel folder

--- admin.php
+++ admin.php
@@ -9034,12 +9034,12 @@ elseif ($user['step'] == "cr_step_get_emoji" && in_array($from_id, $admin_ids)) 
             [
                 ['text' => "⚙️ تنظیمات", 'callback_data' => "iranpay1setting"],
                 ['text' => $arzireyali1status, 'callback_data' => "editpayment-arzireyali1-$arzireyali1"],
-                ['text' => "تتراپی", 'callback_data' => "arzireyali1"],
+                ['text' => "تتراپی 💳", 'callback_data' => "arzireyali1"],
             ],
             [
                 ['text' => "⚙️ تنظیمات", 'callback_data' => "iranpay2setting"],
                 ['text' => $arzireyali2status, 'callback_data' => "editpayment-arzireyali2-$arzireyali2"],
-                ['text' => "ترونادو", 'callback_data' => "arzireyali2"],
+                ['text' => "ترونادو 💳", 'callback_data' => "arzireyali2"],
             ],
             // [
             //     ['text' => "⚙️ تنظیمات", 'callback_data' => "iranpay3setting"],
